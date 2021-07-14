0632a75ec3550ee61da7766c31cd13fe863f17b4
guyjof <guyjofre02@gmail.com>
2021-07-14 17:03:24 +0300

    homepage and header fix

--- src/cmps/stay-filter.vue
+++ src/cmps/stay-filter.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="filter-container">
-    <form action=""></form>
+    <form action="" @submit.prevent>
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <button class="search-btn">
+        <img src="../assets/imgs/search_white_24dp.svg" alt="" />
+      </button>
+    </form>
   </div>
 </template>
 
--- src/views/home.vue
+++ src/views/home.vue
@@ -3,7 +3,35 @@
     <div class="shadow"></div>
     <img class="hero" src="@/assets/imgs/home-page/hero1.jpeg" />
     <stay-filter />
-    <div class="categorys"></div>
+    <div class="categories">
+      <h2>Live anywhere</h2>
+      <div class="cards-container">
+        <div class="card">
+          <img
+            src="https://images.unsplash.com/photo-1431274172761-fca41d930114?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+            alt=""
+          />
+        </div>
+        <div class="card">
+          <img
+            src="https://images.unsplash.com/photo-1431274172761-fca41d930114?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+            alt=""
+          />
+        </div>
+        <div class="card">
+          <img
+            src="https://images.unsplash.com/photo-1431274172761-fca41d930114?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+            alt=""
+          />
+        </div>
+        <div class="card">
+          <img
+            src="https://images.unsplash.com/photo-1431274172761-fca41d930114?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +39,9 @@
 import stayFilter from "../cmps/stay-filter.vue";
 export default {
   name: "Home",
-  data() {},
+  data() {
+    return {};
+  },
   components: {
     stayFilter,
   },
