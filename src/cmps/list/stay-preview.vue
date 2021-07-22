90127ce7576c8f5c9792f4420c7a22026a0582b0
Daniel barak <danielbarak618@gmail.com>
2021-07-22 13:56:15 +0300

    mobile design for stay list

--- src/cmps/list/stay-preview.vue
+++ src/cmps/list/stay-preview.vue
@@ -1,79 +1,79 @@
 <template>
-	<section class="stay-card-container" @click.stop="sendToDetails(stay._id)">
-		<button class="wishlist-btn">
-			<span v-if="!onWishlist" class="material-icons-two-tone">
-				favorite
-			</span>
-			<span v-else class="in-list material-icons-outlined">
-				favorite
-			</span>
-		</button>
-		<el-carousel
-			v-if="stay.imgUrls.length"
-			class="img-carousell"
-			trigger="click"
-			height="170px"
-			arrow="always"
-			:autoplay="false"
-			:loop="true"
-		>
-			<el-carousel-item v-for="(imgUrl, idx) in stay.imgUrls" :key="idx">
-				<img :src="imgUrl" />
-			</el-carousel-item>
-		</el-carousel>
-		<img v-else :src="require('@/assets/imgs/no_img.jpeg')" />
-		<div class="card-rating">
-			<p>
-				<span class="material-icons star-icon"> star </span>{{ avgRate }} (<small>
-					{{ stay.reviews.length }}</small
-				>)
-			</p>
-		</div>
-		<div class="card-location">{{ stay.propertyType }} &middot; {{ city }}</div>
-		<div class="card-name">{{ stay.name }}</div>
-		<div class="card-price">
-			<p>
-				<span>${{ stay.price }}</span
-				>/night
-			</p>
-		</div>
-	</section>
+  <section class="stay-card-container" @click.stop="sendToDetails(stay._id)">
+    <button class="wishlist-btn">
+      <span v-if="!onWishlist" class="material-icons-two-tone">
+        favorite
+      </span>
+      <span v-else class="in-list material-icons-outlined">
+        favorite
+      </span>
+    </button>
+    <el-carousel
+      v-if="stay.imgUrls.length"
+      class="img-carousell"
+      trigger="click"
+      arrow="always"
+      height="170px"
+      :autoplay="false"
+      :loop="true"
+    >
+      <el-carousel-item v-for="(imgUrl, idx) in stay.imgUrls" :key="idx">
+        <img :src="imgUrl" />
+      </el-carousel-item>
+    </el-carousel>
+    <img v-else :src="require('@/assets/imgs/no_img.jpeg')" />
+    <div class="card-rating">
+      <p>
+        <span class="material-icons star-icon"> star </span
+        >{{ avgRate }} (<small> {{ stay.reviews.length }}</small
+        >)
+      </p>
+    </div>
+    <div class="card-location">{{ stay.propertyType }} &middot; {{ city }}</div>
+    <div class="card-name">{{ stay.name }}</div>
+    <div class="card-price">
+      <p>
+        <span>${{ stay.price }}</span
+        >/night
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-	props: {
-		stay: Object,
-	},
-	data() {
-		return {
-			onWishlist: false,
-		}
-	},
-	methods: {
-		sendToDetails(stayId) {
-			this.$router.push(`/stay/${stayId}`)
-		},
-		toggleWishlist(stayId) {
-			this.onWishlist = !this.onWishlist
-			if (this.onWishlist === true) {
-				console.log('added to wish list', stayId)
-			}
-		},
-	},
-	computed: {
-		city() {
-			return this.stay.loc.address.split(',')[0]
-		},
-		avgRate() {
-			if (!this.stay.reviews.length) return 0
-			const sum = this.stay.reviews.reduce((acc, review) => {
-				return acc + review.rate
-			}, 0)
-			return (sum / this.stay.reviews.length).toFixed(1)
-		},
-	},
-	created() {},
+  props: {
+    stay: Object,
+  },
+  data() {
+    return {
+      onWishlist: false,
+    }
+  },
+  methods: {
+    sendToDetails(stayId) {
+      this.$router.push(`/stay/${stayId}`)
+    },
+    toggleWishlist(stayId) {
+      this.onWishlist = !this.onWishlist
+      if (this.onWishlist === true) {
+        console.log('added to wish list', stayId)
+      }
+    },
+  },
+  computed: {
+    city() {
+      return this.stay.loc.address.split(',')[0]
+    },
+    avgRate() {
+      if (!this.stay.reviews.length) return 0
+      const sum = this.stay.reviews.reduce((acc, review) => {
+        return acc + review.rate
+      }, 0)
+      return (sum / this.stay.reviews.length).toFixed(1)
+    },
+  },
+  created() {},
 }
 </script>
 
