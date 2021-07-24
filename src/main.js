f36928618a63608f57783f309ceb866c5a01af93
guyjof <guyjofre02@gmail.com>
2021-07-24 19:56:22 +0300

    stay checkout on mobile

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import 'vue-loaders/dist/vue-loaders.css'
 import VueLoaders from 'vue-loaders'
 import VueSocialSharing from 'vue-social-sharing'
 import VueClipboard from 'vue-clipboard2'
+import 'vue-datetime/dist/vue-datetime.css'
 import GoogleAuth from '@/config/google_oAuth.js'
 const gauthOption = {
 	clientId: '707231563844-e5cpkqrlt62gncmj6b84of5sml9lp8g9.apps.googleusercontent.com',
